b1caf28b3fc917c13368471c0809dbc61906bb1c
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-12-12 20:44:50 +0600

    Installed mongodb and connected with mongodb server

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 app.use(cors());
 //Body parser
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
+
 const users = [
     {id: 1, name: 'Monirul', email: 'monirul@gmail.com'},
     {id: 2, name: 'Javed', email: 'javed@gmail.com'},
     {id: 3, name: 'Palton', email: 'palton@gmail.com'}
 ]
+
+// DBUser : simpleNodeServer
+// Password: s03y1QMMwAGYzpNi
+
+const uri = "mongodb+srv://simpleNodeServer:s03y1QMMwAGYzpNi@cluster0.ak6zw.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("simpleNode").collection("users");
+  // perform actions on the collection object
+  console.log('Database connected successfully');
+  client.close();
+});
+
 
 app.get('/', (req, res) =>{
     res.send('Welcome');
